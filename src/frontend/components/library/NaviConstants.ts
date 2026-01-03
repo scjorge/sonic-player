56455521505ee475b613112ccf23ca89af6f1297
Jorge Costa <jorgesilva.ti@hotmail.com>
2026-01-02 21:32:05 -0300

    ok

--- src/frontend/components/library/NaviConstants.ts
+++ src/frontend/components/library/NaviConstants.ts
@@ -26,7 +26,7 @@ export const NAVI_COLUMN_CONFIG: ColumnConfig[] = [
 ];
 
 export const NAVI_DOWNLOAD_COLUMN_CONFIG: ColumnConfig[] = [
-  { id: 'select', label: '', width: 50, visible: true, minWidth: 50 },
+  //{ id: 'select', label: '', width: 50, visible: true, minWidth: 50 },
   { id: 'index', label: '#', width: 40, visible: true, minWidth: 30 },
   { id: 'cover', label: 'Capa', width: 80, visible: true, minWidth: 50 },
   { id: 'combinedTitle', label: 'Título combinado', width: 300, visible: false, minWidth: 150 },
